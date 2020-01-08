e381e6839242028840152f4008c6cf637b6618fc
anitejngo <laogdo@gmail.com>
2020-01-08 18:44:09 +0100

    Connect to db and create table if it does not exist

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const webscraper = require('web-scraper-js');
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
 
 const baseUrl = 'https://www.olx.ba/pretraga?vrsta=samoizdavanje&kategorija=23&kanton=15&grad%5B0%5D=1088&stranica=';
 const articleDetailsUrl = 'https://www.olx.ba/artikal/';
+
+const createTableQuery = 'CREATE TABLE apartments(id SERIAL PRIMARY KEY, name VARCHAR(200), price VARCHAR(200),link VARCHAR(200));';
+const tableExistsQuery = 'SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = \'public\' AND table_name = \'apartments\');';
 
 (async () => {
 
@@ -44,25 +47,23 @@ const articleDetailsUrl = 'https://www.olx.ba/artikal/';
 		})
 	).then(result => result.flat().filter(article => article.id))
 
-	console.log('ARTICLES:', articles);
-
-	console.log('Connecting to db!');
-
 	const client = new Client({
 		connectionString: process.env.DATABASE_URL,
+		ssl: true,
 	});
 
 	client.connect();
-	client
-		.query('\'SELECT NOW() as now\'')
-		.then(res => console.log(res.rows[0]))
-		.catch(e => console.error(e))
-	client.end();
 
-	console.log('END');
-	// client.query('SELECT NOW()', (err, res) => {
-	// 	console.log(err, res)
-	// 	client.end()
-	// })
+	client
+		.query(tableExistsQuery)
+		.then(res => {
+				if (!res.rows[0].exists) {
+					console.log('Create database!');
+					client.query(createTableQuery)
+				}
+				client.end()
+			}
+		)
+		.catch(e => console.error(e.stack));
 
 })();
