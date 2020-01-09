d6521dc4fd9106d0239df193525a90c2a7bdd2be
anitejngo <laogdo@gmail.com>
2020-01-09 16:21:32 +0100

    Send email if new ads found

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const webscraper = require('web-scraper-js');
 const { Client } = require('pg');
-const _ = require('lodash');
+const sgMail = require('@sendgrid/mail');
 
 const baseUrl = 'https://www.olx.ba/pretraga?vrsta=samoizdavanje&kategorija=23&kanton=15&grad%5B0%5D=1088&stranica=';
 const articleDetailsUrl = 'https://www.olx.ba/artikal/';
 
 const createTableQuery = 'CREATE TABLE apartments(id SERIAL PRIMARY KEY, name VARCHAR(200), price VARCHAR(200),link VARCHAR(200));';
 const tableExistsQuery = 'SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = \'public\' AND table_name = \'apartments\');';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 (async () => {
 
@@ -55,7 +57,10 @@ const tableExistsQuery = 'SELECT EXISTS(SELECT * FROM information_schema.tables 
 
 	client.connect();
 
-	await  client
+	await deleteApartments(client);
+
+
+	await client
 		.query(tableExistsQuery)
 		.then(res => {
 				if (!res.rows[0].exists) {
@@ -72,20 +77,22 @@ const tableExistsQuery = 'SELECT EXISTS(SELECT * FROM information_schema.tables 
 
 	const currentApartments = await getApartments(client);
 
-	const newApartments = _.xorBy(currentApartments, scrapedApartments, 'id');
+	const newApartments = xorBy(currentApartments, scrapedApartments, 'id');
 
 	console.log('scraped apartments length:', scrapedApartments.length);
 	console.log('current apartments length:', currentApartments.length);
 	console.log('new apartments length:', newApartments.length);
 	//TODO send intersection via email!
 
-	await deleteApartments(client);
-
+	if (newApartments.length) {
+		sendEmail(newApartments);
+	}
+	
 	await Promise.all(
 		scrapedApartments.map(async apartment => {
 			await updateApartment(client, apartment)
 		})
-	);
+	).catch();
 
 	client.end()
 
@@ -109,3 +116,25 @@ const getApartments = async client => {
 const deleteApartments = async client => {
 	await client.query('TRUNCATE apartments;').catch();
 };
+
+const xorBy = (currentApartments, scrapedApartments, fieldName) => {
+	if (currentApartments.length < 1) {
+		return scrapedApartments
+	} else {
+		const fn = it => it[fieldName];
+		const compareSet = new Set(scrapedApartments.map(fn));
+		return currentApartments.map(fn).filter(el => !compareSet.has(el));
+	}
+};
+
+const sendEmail = (newApartments) => {
+
+		const msg = {
+			to: 'laogdo@gmail.com',
+			from: 'laogdo@gmail.com',
+			subject: 'We found new apartments, check it out!',
+			html: `Here is the list of new apartments: <ul>${newApartments.map(apartment => `<li>${apartment.link}</li>`)}</ul>`
+		};
+		sgMail.send(msg);
+	}
+;
